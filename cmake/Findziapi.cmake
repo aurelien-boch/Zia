369aab18b36d1e9135921a61ce77f76527afc821
Aurélien BOCH <aurelien.boch@epitech.eu>
2022-02-01 13:51:15 +0100

    feat: add elected API

---
+++ cmake/Findziapi.cmake
@@ -0,0 +1,16 @@
+if (NOT ziapi_found)
+    include(ExternalProject)
+    ExternalProject_Add(
+            ziapi_git
+            GIT_REPOSITORY  https://github.com/martin-olivier/ZiAPI.git
+            GIT_TAG         v2.0.0
+            INSTALL_COMMAND ""
+            TEST_COMMAND    ""
+    )
+    add_library(ziapi INTERFACE)
+
+    add_dependencies(ziapi INTERFACE ziapi_git)
+    ExternalProject_Get_Property(ziapi_git SOURCE_DIR)
+    target_include_directories(ziapi INTERFACE "${SOURCE_DIR}/include")
+    set(ziapi_found)
+endif()
