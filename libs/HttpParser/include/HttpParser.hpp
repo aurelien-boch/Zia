2049246215c32f1c3d5b1da1cd748a0bfe760100
Benjamin Reigner <benjamin.reigner@epitech.eu>
2022-02-02 13:28:51 +0100

    feat: setup class and CMakeLists
    Everything is set up for this library in order to have it compile along
    with the core of our program.

---
+++ libs/HttpParser/include/HttpParser.hpp
@@ -0,0 +1,14 @@
+#ifndef ZIA_HTTPPARSER_HPP
+#define ZIA_HTTPPARSER_HPP
+
+class HttpParser {
+
+public:
+
+    HttpParser(HttpParser &other) = delete;
+    HttpParser operator=(HttpParser &other) = delete;
+
+    HttpParser() = default;
+};
+
+#endif /* ZIA_HTTPPARSER_HPP */
