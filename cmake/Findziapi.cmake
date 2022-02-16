cd35cde7a2de4643fbefdb9d027caec76e58dd60
Victoire RABEAU <victoire.rabeau@epitech.eu>
2022-02-16 14:22:24 +0100

    fix(CMake): Fix findziapi.cmake

--- cmake/Findziapi.cmake
+++ cmake/Findziapi.cmake
@@ -1,16 +1,16 @@
 if (NOT ziapi_found)
-    include(ExternalProject)
-    ExternalProject_Add(
+    include(FetchContent)
+    FetchContent_Declare(
             ziapi_git
             GIT_REPOSITORY  https://github.com/martin-olivier/ZiAPI.git
             GIT_TAG         v3.1.1
             INSTALL_COMMAND ""
             TEST_COMMAND    ""
     )
-    add_library(ziapi INTERFACE)
 
-    add_dependencies(ziapi INTERFACE ziapi_git)
-    ExternalProject_Get_Property(ziapi_git SOURCE_DIR)
-    target_include_directories(ziapi INTERFACE "${SOURCE_DIR}/include")
+    FetchContent_MakeAvailable(ziapi_git)
+    add_library(ziapi INTERFACE)
+    add_dependencies(ziapi PUBLIC ziapi_git)
+    target_include_directories(ziapi INTERFACE "${ziapi_SOURCE_DIR}/include")
     set(ziapi_found)
 endif()
