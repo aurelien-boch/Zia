cd098a224e6e526110d694caca2abca49c3dd4e1
Paul Beaudet <paul.beaudet@epitech.eu>
2022-02-17 13:54:51 +0000

    feat(config_parser): Add getter for any parameters in the config

--- libs/config_parser/include/ConfigParser.hpp
+++ libs/config_parser/include/ConfigParser.hpp
@@ -2,24 +2,38 @@
 #define CONFIG_PARSER_HPP
 
 #include <iostream>
-#include <unordered_map>
-#include <initializer_list>
+
 #include <yaml-cpp/yaml.h>
+
 #include <ziapi/Config.hpp>
 
-namespace configParser
+namespace parser
 {
     class ConfigParser
     {
         public:
             ConfigParser(std::string const &path);
+
             ~ConfigParser() = default;
-            std::unordered_map<std::string, std::unordered_map<std::string, std::string>> const &getConfigMap() const;
+            
+            // doc missing
+            [[nodiscard]] inline ziapi::config::Dict const &getConfigMap() const { return (_configMap); }
+            
+            // doc missing
             void printConfig();
 
+            // doc missing
+            [[nodiscard]] ziapi::config::Dict operator[](const std::string &moduleName);
+
+            /*
+             * Doc missing
+             */
+            [[nodiscard]] std::shared_ptr<ziapi::config::Node> getValue(const std::string &root, const std::string &module, const std::string &param);
+
         private:
-            void _checkConfig();
+            inline void _checkConfig();
             void _putConfigInMap();
+
             YAML::Node _config;
             ziapi::config::Dict _configMap;
     };
--- libs/config_parser/src/ConfigParser.cpp
+++ libs/config_parser/src/ConfigParser.cpp
@@ -1,20 +1,54 @@
 #include "ConfigParser.hpp"
 
-namespace configParser
+namespace parser
 {
-
-    ConfigParser::ConfigParser(std::string const &path)
+    ConfigParser::ConfigParser(std::string const &path) // doc missing
     {
         try {
             _config = YAML::LoadFile(path);
         } catch(const std::exception& e) {
-            std::cerr << "[ConfigParser] " << e.what() << '\n';
+            std::cerr << "[ConfigParser] " << e.what() << std::endl;
         }
         _checkConfig();
         _putConfigInMap();
     }
 
-    void ConfigParser::_checkConfig()
+    void ConfigParser::printConfig()
+    {
+        for (const auto &[masterKey, masterValue] : _configMap) {
+            std::cout << masterKey << ": \n";
+            for (const auto &[key, value] : masterValue->AsDict()) {
+                std::cout << "\t"<< key << ": \n";
+                for (const auto &[parameterKey, parameterValue] : value->AsDict()) {
+                    if (parameterValue->index() == ziapi::config::Type::kInt) {
+                        std::cout << "\t\t" << parameterKey << ": " << parameterValue->AsInt() << std::endl; 
+                        continue;
+                    }
+                    std::cout << "\t\t" << parameterKey << ": " << parameterValue->AsString() << std::endl;
+                }
+            }
+        }
+    }
+
+    ziapi::config::Dict ConfigParser::operator[](const std::string &moduleName)
+    {
+        if (_configMap["modules"]->AsDict().find(moduleName) == _configMap["modules"]->AsDict().end())
+            throw std::range_error("[ConfigParser] not in [modules]");
+        return (_configMap["modules"]->AsDict()[moduleName]->AsDict());
+    }
+
+    std::shared_ptr<ziapi::config::Node> ConfigParser::getValue(const std::string &root, const std::string &module, const std::string &param) 
+    {
+        if (_configMap.find(root) == _configMap.end())
+            throw std::range_error("[ConfigParser] not in config");
+        if (_configMap[root]->AsDict().find(module) == _configMap[root]->AsDict().end())
+            throw std::range_error("[ConfigParser] not in [" + root + "]");
+        if (_configMap[root]->AsDict()[module]->AsDict().find(param) == _configMap[root]->AsDict()[module]->AsDict().end())
+            throw std::range_error("[ConfigParser] not in [" + root + "][" + module + "]");
+        return (_configMap[root]->AsDict()[module]->AsDict()[param]);
+    }
+
+    inline void ConfigParser::_checkConfig()
     {
         if (_config.IsNull())
             throw std::runtime_error("[ConfigParser] Config file not found");
@@ -22,15 +56,20 @@ namespace configParser
 
     void ConfigParser::_putConfigInMap()
     {
-        for (auto const &section : _config) { // modules
+        for (auto const &section : _config) {
             ziapi::config::Dict modules;
-            for (auto const &moduleName : section.second) { // phpcgi, tls
+            for (auto const &moduleName : section.second) {
                 ziapi::config::Dict tmp;
 
-                for (auto const &parameters : moduleName.second) { // ip : 192, foo : bar
-                    if (!parameters.second.IsDefined()) {
+                for (auto const &parameters : moduleName.second) {
+                    if (!parameters.second.IsDefined())
                         throw std::runtime_error("[ConfigParser] Parameter undefined");
+                    try {
+                        tmp[parameters.first.as<std::string>()] = std::make_shared<ziapi::config::Node>(parameters.second.as<int>());
+                        continue;
+                    } catch(const YAML::BadConversion &e) {
                     }
+                    // TODO Add a case for sequences
                     tmp[parameters.first.as<std::string>()] = std::make_shared<ziapi::config::Node>(parameters.second.as<std::string>());
                 }
                 modules[moduleName.first.as<std::string>()] = std::make_shared<ziapi::config::Node>(tmp);
@@ -39,16 +78,4 @@ namespace configParser
         }
     }
 
-    void ConfigParser::printConfig()
-    {
-        for (const auto &[masterKey, masterValue] : _configMap) {
-            std::cout << masterKey << ": \n";
-            for (const auto &[key, value] : masterValue.get()->AsDict()) {
-                std::cout << "\t"<< key << ": \n";
-                for (const auto &[parameterKey, parameterValue] : value.get()->AsDict()) {
-                    std::cout << "\t\t" << parameterKey << ": " << parameterValue.get()->AsString() << std::endl;
-                }
-            }
-        }
-    }
 }
