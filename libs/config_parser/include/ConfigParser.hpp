ea93b8dc5e0a2f28eebd0e4fc3e16cd3339b1cf1
Paul Beaudet <paul.beaudet@epitech.eu>
2022-02-25 18:08:50 +0000

    feat(config_parser): Change the return of getConfigsPaths
    The function now return a std::vector of LibInfo struct which contain the name, the path to the config and the path to the lib.so file

--- libs/config_parser/include/ConfigParser.hpp
+++ libs/config_parser/include/ConfigParser.hpp
@@ -1,79 +1,93 @@
 #ifndef CONFIG_PARSER_HPP
 #define CONFIG_PARSER_HPP
 
-#include <iostream>
 #include <fstream>
-
+#include <iostream>
 #include <yaml-cpp/yaml.h>
-
 #include <ziapi/Config.hpp>
 
 namespace parser
 {
+    struct LibInfo {
+        std::string name;
+        std::string configPath;
+        std::string modulePath;
+    };
+
     class ConfigParser
     {
-        public:
-            ConfigParser(std::string const &path);
+      public:
+        ConfigParser(std::string const &path);
 
-            ~ConfigParser() = default;
+        ~ConfigParser() = default;
 
-            /*
-             * @brief Parses the config file
-             * In case of error it prints the error
-             * @param[in] newPath The path to the config file
-             * @throw std::runtime_error if there is an error while parsing the config file
-             */
-            void parse(std::string const &newPath);
-            
-            /*
-             * @brief Returns the config map
-             * @return The config map
-             */
-            [[nodiscard]] inline ziapi::config::Dict const &getConfigMap() const { return (_configMap); }
+        /*
+         * @brief Parses the config file
+         * In case of error it prints the error
+         * @param[in] newPath The path to the config file
+         * @throw std::runtime_error if there is an error while parsing the config file
+         */
+        void parse(std::string const &newPath);
 
-            /*
-             * @brief Overloads the [] operator to access the unordered_map of the module
-             * @throw std::range_error if the module is not in the config
-             * @return std::unordered_map<std::string, std::shared_ptr<ziapi::config::Node>> of the module
-             */
-            [[nodiscard]] ziapi::config::Dict operator[](std::string const &moduleName) const;
+        /*
+         * @brief Returns the config map
+         * @return The config map
+         */
+        [[nodiscard]] inline ziapi::config::Dict const &getConfigMap() const { return (_configMap); }
 
-            /*
-             * @brief Returns the value of a parameter in the module
-             * Use As[String, Int, Double, Bool, Dict, Array] to access the right value
-             * @param[in] root Root of the config
-             * @param[in] module Module name in the root element of the config
-             * @param[in] param Parameter in the module int the root element of the config
-             * @return A shared pointer to the parameter
-             */
-            [[nodiscard]] std::shared_ptr<ziapi::config::Node> getValue(std::string const &root, std::string const &module, std::string const &param);
+        /*
+         * @brief Overloads the [] operator to access the unordered_map of the module
+         * @throw std::range_error if the module is not in the config
+         * @return std::unordered_map<std::string, std::shared_ptr<ziapi::config::Node>> of the module
+         */
+        [[nodiscard]] ziapi::config::Dict operator[](std::string const &moduleName) const;
 
-            /*
-             * @brief Retrieves a vector to the path for every config file
-             * @param[in] Path to the file containing every paths
-             * @throw std::runtime_error if the config file is not found
-             * @return A vector of string
-             */
-            [[nodiscard]] static const std::vector<std::string> getConfigsPaths(std::string const &filename)
-            {
-                std::vector<std::string> configPaths;
-                std::ifstream file(filename);
-                std::string line;
+        /*
+         * @brief Returns the value of a parameter in the module
+         * Use As[String, Int, Double, Bool, Dict, Array] to access the right value
+         * @param[in] root Root of the config
+         * @param[in] module Module name in the root element of the config
+         * @param[in] param Parameter in the module int the root element of the config
+         * @return A shared pointer to the parameter
+         */
+        [[nodiscard]] std::shared_ptr<ziapi::config::Node> getValue(std::string const &root, std::string const &module,
+                                                                    std::string const &param);
 
-                if (!file.is_open())
-                    throw std::runtime_error("[ConfigParser] Root to config file not found");
-                while (std::getline(file, line))
-                    configPaths.emplace_back(line);
-                return (configPaths);
+        /*
+         * @brief Retrieves a vector to the path for every config file
+         * @param[in] Path to the file containing every paths
+         * @throw std::runtime_error if the config file is not found
+         * @return A vector of string
+         */
+        [[nodiscard]] static const std::vector<LibInfo> getConfigsPaths(std::string const &filename)
+        {
+            std::vector<LibInfo> libsInfos;
+            std::ifstream file(filename);
+            std::string line;
+
+            if (!file.is_open())
+                throw std::runtime_error("[ConfigParser] Root to config file not found");
+            while (std::getline(file, line)) {
+                std::size_t nameEndOffset = line.find_first_of("::");
+                if (nameEndOffset == std::string::npos)
+                    throw std::runtime_error("[ConfigParser] Error while parsing config file");
+                std::size_t configPathOffset = line.find_first_of("::", nameEndOffset + 2);
+                if (configPathOffset == std::string::npos)
+                    throw std::runtime_error("[ConfigParser] Error while parsing config file");
+                libsInfos.emplace_back(LibInfo{line.substr(0, nameEndOffset),
+                                               line.substr(nameEndOffset + 2, configPathOffset - nameEndOffset - 2),
+                                               line.substr(configPathOffset + 2)});
             }
+            return (libsInfos);
+        }
 
-        private:
-            inline void _checkConfig();
-            void _putConfigInMap();
+      private:
+        inline void _checkConfig();
+        void _putConfigInMap();
 
-            YAML::Node _config;
-            ziapi::config::Dict _configMap;
+        YAML::Node _config;
+        ziapi::config::Dict _configMap;
     };
-}
+} // namespace parser
 
 #endif // CONFIG_PARSER_HPP
--- libs/config_parser/tests/testsConfigParser.cpp
+++ libs/config_parser/tests/testsConfigParser.cpp
@@ -1,17 +1,21 @@
-#include <catch2/catch_test_macros.hpp>
-
 #include "ConfigParser.hpp"
+
+#include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("static parsing of config paths", "[config_parser]")
 {
     auto paths = parser::ConfigParser::getConfigsPaths("../libs/config_parser/tests/test_file.txt");
 
     REQUIRE(paths.size() == 1);
+    REQUIRE(paths[0].name == "basic");
+    REQUIRE(paths[0].configPath == "../config.yml");
+    REQUIRE(paths[0].modulePath == "./lib/libmock_lib.so");
 }
 
 TEST_CASE("static parsing error : file not found", "[config_parser]")
 {
-    REQUIRE_THROWS_AS(parser::ConfigParser::getConfigsPaths("../libs/config_parser/tests/test_file_not_found.txt"), std::runtime_error);
+    REQUIRE_THROWS_AS(parser::ConfigParser::getConfigsPaths("../libs/config_parser/tests/test_file_not_found.txt"),
+                      std::runtime_error);
 }
 
 TEST_CASE("creation of a config parser and check of a valid value in the config", "[config_parser]")
@@ -28,14 +32,16 @@ TEST_CASE("creation of a config parser and check of an invalid value in the conf
     REQUIRE_THROWS_AS(parser.getValue("modules", "phpCGI", "foot")->AsString(), std::range_error);
 }
 
-TEST_CASE("creation of a config parser and check of a valid value in the config with an invalid root", "[config_parser]")
+TEST_CASE("creation of a config parser and check of a valid value in the config with an invalid root",
+          "[config_parser]")
 {
     parser::ConfigParser parser("../libs/config_parser/tests/config.yml");
 
     REQUIRE_THROWS_AS(parser.getValue("module", "phpCGI", "foo")->AsString(), std::range_error);
 }
 
-TEST_CASE("creation of a config parser and check of a valid value in the config with an invalid module", "[config_parser]")
+TEST_CASE("creation of a config parser and check of a valid value in the config with an invalid module",
+          "[config_parser]")
 {
     parser::ConfigParser parser("../libs/config_parser/tests/config.yml");
 
